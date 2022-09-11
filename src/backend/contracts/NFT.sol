4dd6842339d7356c4acd6bda71d77f11a9a6cdc7
Nadai <112663528+Nadai2010@users.noreply.github.com>
2022-09-11 21:27:44 +0100

    Update NFT.sol

--- src/backend/contracts/NFT.sol
+++ src/backend/contracts/NFT.sol
@@ -1,17 +1,16 @@
-// SPDX-License-identifier: MIT
-pragma solidity ^0.8.15;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.14;
 
-import "@openzeppelin/contract/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract NFT is ERC721URIStorage {
-    unit public tokenCount;
+    uint public tokenCount;
 
-    constructor() ERC721("NadaiMarketPlace","NAI") {}
-
-    function mint(string memory _tokenURI) external returns(unit) {
+    constructor() ERC721("NadaiMarketPlace", "NFI") {}
+    function mint(string memory _tokenURI) external returns(uint) {
         tokenCount ++;
         _safeMint(msg.sender, tokenCount);
         _setTokenURI(tokenCount, _tokenURI);
         return(tokenCount);
     }
-}
+}   
